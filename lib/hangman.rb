bb1b3228c523e6930da1a21ab850918d953bf61c
haiduk <hwlee91@gmail.com>
2020-07-27 02:47:32 -0400

    selects secret word between 5-12 letters from list

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,1 +1,26 @@
-require '5desk'
+
+module HangmanFormulae
+end
+
+class Squares
+end
+
+class Hangman 
+
+  def initialize
+  end
+
+end
+
+original_list = File.open("5desk.txt")
+hidden_word_list = []
+
+original_list.each do |line|
+  if line.strip.length > 4 && line.strip.length < 13
+    hidden_word_list.push(line)
+  end
+end
+
+secret_word = hidden_word_list.sample
+
+puts secret_word
